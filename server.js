261620958d51f9aabfa178ffd3204b158e6afbac
Timothy Kenneth D. Dantic <timothykennethdantic@gmail.com>
2025-01-12 00:58:55 +0800

    last

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://tomohirokenshi.github.io"],
+    origin: ["https://tomohirokenshi.github.io/portfolio/"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
   })
@@ -59,6 +59,8 @@ ${message}
     });
   }
 });
+
+app.options("/api/contact", cors());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
