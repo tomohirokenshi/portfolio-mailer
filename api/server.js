562d8d78e0bca4a4c073bc7b00eea8435746dbf8
Timothy Kenneth D. Dantic <timothykennethdantic@gmail.com>
2025-01-11 21:57:48 +0800

    CORS

--- api/server.js
+++ api/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tomohirokenshi.github.io/portfolio/",
+  })
+);
 
 app.post("/api/contact", async (req, res) => {
   const { name, email, number, message } = req.body;
