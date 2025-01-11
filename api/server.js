c3d8c6d93d15aba67f2eeb3a18a2742ffaa5d1db
Timothy Kenneth D. Dantic <timothykennethdantic@gmail.com>
2025-01-11 21:40:50 +0800

    First Commit

---
+++ api/server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/api/contact", async (req, res) => {
+  const { name, email, number, message } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: process.env.EMAIL_USER,
+      subject: `New Message from ${name}`,
+      text: `
+You have received a new message from 
+${name}
+${email} | ${number}
+
+${message}
+    `,
+      replyTo: email,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(200).send("Message sent successfully!");
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Failed to send message.");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
